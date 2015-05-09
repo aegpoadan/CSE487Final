78479fdfc221cb26fb3ba20cf2ea9818e8db055d
aegpoadan <ahayes2425@gmail.com>
2015-05-09 15:10:51 -0400

    Added Time Slow Package
    Added assets needed to have Time Slow weapon in-game. Slows down the
    “enemySpeed” variable in the “GlobalData” script that is attached to
    the game manager

---
+++ Scripts/TimeSlow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeSlow : MonoBehaviour {
+	private GameObject gameManager;
+	private GlobalData globalData;
+
+	void Awake() {
+		gameManager = GameObject.Find ("GameManager");
+		globalData = gameManager.GetComponent<GlobalData> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("slowEnemies");
+		Destroy (gameObject, 5.1f);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator slowEnemies() {
+		print ("Slowing enemies!");
+		globalData.enemySpeed = .5f;	//Reduce enemy speed to half of original value
+		yield return new WaitForSeconds(5.0f); //Enemies are slowed for 5 seconds
+		globalData.enemySpeed = 1.0f;
+		print ("Enemies are back at normal sped");
+	}
+}
